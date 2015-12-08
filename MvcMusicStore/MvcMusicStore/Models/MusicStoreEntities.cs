99f6efcdedac807d89867d4b89bb6396eb82d290
sellnat77 <sell_nat@yahoo.com>
2015-12-07 18:24:21 -0800

    Finished shopping cart step 8

--- MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 
 namespace MvcMusicStore.Models
 {
@@ -10,7 +6,16 @@ namespace MvcMusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
-        public System.Data.Entity.DbSet<MvcMusicStore.Models.Artist> Artists { get; set; }
+        public DbSet<Artist> Artists
+        {
+            get;
+            set;
+        }
+        public DbSet<Cart>
+Carts { get; set; }
+        public DbSet<Order> Orders
+        { get; set; }
+        public DbSet<OrderDetail>
+OrderDetails { get; set; }
     }
 }
