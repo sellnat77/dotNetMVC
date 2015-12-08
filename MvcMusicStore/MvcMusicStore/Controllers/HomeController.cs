9d7539202930f9d49e38fcb1330c86f498d84419
sellnat77 <sell_nat@yahoo.com>
2015-12-07 18:55:01 -0800

    Complete with project, admin portion only step left out

--- MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
@@ -10,9 +9,22 @@ namespace MvcMusicStore.Controllers
     {
         //
         // GET: /Home/
+        MusicStoreEntities storeDB = new MusicStoreEntities();
         public ActionResult Index()
         {
-            return View();
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
         }
     }
 }
